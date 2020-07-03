a35731eda515d3830082b8137d74096996406583
Sandra Grahl <grahlsan@gmail.com>
2020-07-03 20:16:35 +0100

    changes to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -31,7 +31,7 @@ const Footer = () => (
           rel="noopener noreferrer"
           aria-label="Listen on Spotify"
         >
-          <Spotify size="22" aria-hidden="true"/>
+          <Spotify size="22" aria-hidden="true" />
 
         </ListenIcon>
         <ListenIcon
@@ -58,7 +58,7 @@ const Footer = () => (
           rel="noopener noreferrer"
           aria-label="Listen on Google Play"
         >
-          <Googlepodcasts size="21" style={{ transform: `translateY(-4px)`,  marginLeft: `2px`,}} aria-hidden="true" />
+          <Googlepodcasts size="21" style={{ transform: `translateY(-4px)`, marginLeft: `2px`, }} aria-hidden="true" />
         </ListenIcon>
         <ListenIcon
           href="https://castbox.fm/channel/Midweek-Murders-id3009183?utm_source=website&utm_medium=dlink&utm_campaign=web_share&utm_content=Midweek%20Murders-CastBox_FM"
@@ -66,7 +66,7 @@ const Footer = () => (
           rel="noopener noreferrer"
           aria-label="Listen on Castbox"
         >
-          <Castbox size="22" style={{ transform: `translateY(-3px)`, marginLeft: `-1px`,}} aria-hidden="true"/>
+          <Castbox size="22" style={{ transform: `translateY(-3px)`, marginLeft: `-1px`, }} aria-hidden="true" />
         </ListenIcon>
         <ListenIcon
           href="https://podcasts.apple.com/gb/podcast/midweek-murders/id1518983516"
@@ -74,7 +74,7 @@ const Footer = () => (
           rel="noopener noreferrer"
           aria-label="Listen on Apple"
         >
-          <Podcast size="22" style={{ transform: `translateY(1px)`, }} aria-hidden="true"/>
+          <Podcast size="22" style={{ transform: `translateY(1px)`, }} aria-hidden="true" />
 
         </ListenIcon>
       </Text>
@@ -98,7 +98,7 @@ const Footer = () => (
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Follow us on Instagram"
-          >
+        >
           <InstagramWithCircle
             size="22"
             aria-hidden="true"
@@ -109,7 +109,7 @@ const Footer = () => (
   </Text2>
     </Contact>
 
-    <div style={{ flexBasis: `70%`, margin: `200px 15% 2% 15%`, overflow: `hidden`, textAlign: `center` }}>
+    <div style={{ flexBasis: `80%`, margin: `200px 10% 2% 10%`, overflow: `hidden`, textAlign: `center` }}>
 
       <Thumbnail><ImgStyle /></Thumbnail>
       <NavFooter>
