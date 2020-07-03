8242cf26b1ef23330ca6ae797bc826207734c6f4
Sandra Grahl <grahlsan@gmail.com>
2020-07-03 20:00:42 +0100

    trying to improve preformance

--- src/components/layout.js
+++ src/components/layout.js
@@ -53,7 +53,7 @@ const Layout = ({ children }) => {
         ]}
       >
         <html lang="en" />
-        <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css' />
 
       </Helmet>
 
